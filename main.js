fe782782d94609d165094ea4bf34c3d1f678b57d
Nicolas Gomez <nicolasgabrielgomez75@gmail.com>
2026-02-03 20:21:58 -0300

    Martes

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+let Btn_menu = document.getElementById("btn_Menu");
+let menu = document.getElementById("menu");
+
+Btn_menu.addEventListener("click",()=>{
+
+ controlar ()
+    
+
+
+
+} )
+
+function controlar() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+  } else {
+    menu.classList.add("active");
+  }
+}
+
+
+console.log(Btn_menu + " este es el elemento capturado del dom" )
+

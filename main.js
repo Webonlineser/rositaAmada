96b203c78a0ff3bd7cbc9a1232c3a84c7942aae9
Nicolas Gomez <nicolasgabrielgomez75@gmail.com>
2026-02-27 17:22:44 -0300

    avance27

--- main.js
+++ main.js
@@ -28,6 +28,21 @@ console.log(Btn_menu + " este es el elemento capturado del dom" )
 
 
 
+let btnCerrar = document.getElementById("btn_cerrar");
+
+btnCerrar.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+
+Btn_menu.addEventListener("click", ()=>{
+
+    menu.classList.toggle("activo");
+
+    document.body.classList.toggle("no-scroll"); // ← agregás esta línea
+
+});
+
 
 /*Animaciones*/
 
